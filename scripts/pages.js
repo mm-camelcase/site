d5be1da0133519d88fae984003143ce5e8a2958e
Mark Mitchell <markmitchell451@gmail.com>
2024-12-07 15:54:37 +0000

    init

--- scripts/pages.js
+++ scripts/pages.js
@@ -3,7 +3,7 @@ const build_path = './out';
 
 const args = process.argv.slice(2);
 const username = args[0];
-const repoURL = `https://github.com/${username}/${username}.github.io.git`;
+const repoURL = `git@github.com:${username}/${username}.github.io.git`;
 const branch = args[1];
 
 console.log(`Deploying to ${repoURL} branch ${branch}`);
